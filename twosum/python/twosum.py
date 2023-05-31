626cf6ac7249a84f93f1eeb9d0fc469133c19a83
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-05-31 14:23:34 -0400

    Adding docstring into twosum function

---
+++ twosum/python/twosum.py
@@ -0,0 +1,21 @@
+
+def twoSum(array, target):
+    """ Initial attempt at solving two sum problem.
+
+    Iterating through the array twice yields a O(n^2) solution. We can do
+    better.
+
+    Args:
+        array (list): List of ints to search for sum.
+        target (int): Int value we are searching for.
+
+    Examples:
+        >>> array = [0, 6, 2, 7, 12]
+        >>> target = 13
+        >>> twoSum(array, target)
+        [1, 3]
+
+    Returns:
+        list of indexes containing the two numbers that sum to our target.
+    """
+    pass
