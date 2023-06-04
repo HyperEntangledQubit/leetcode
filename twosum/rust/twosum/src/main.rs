5beef85d2801b4f3b39b314d430c34f501d80162
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-03 22:13:50 -0400

    Adding rust solution for twosum

---
+++ twosum/rust/twosum/src/main.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+/// Solution for twosum problem in rust
+
+/// Solution using double loop O(n^2) here.
+fn two_sum_dbl_loop(nums: Vec<i32>, target: i32) -> Vec<i32> {
+
+}
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
