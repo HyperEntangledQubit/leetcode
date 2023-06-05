5172d6b8ad4571fc1f0c95b6a0ebfa866972c660
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-04 22:55:56 -0400

    Adding method for twosum using hash

--- twosum/cpp/twosum.cpp
+++ twosum/cpp/twosum.cpp
@@ -22,10 +22,21 @@ std::vector<int> two_sum_dbl_loop(std::vector<int>& nums, int target) {
     return twosum;
 }
 
-void two_sum_hash(std::vector<int>& nums, int target) {
-        std::map<int, int> seen_vals;
-}
+std::vector<int> two_sum_hash(std::vector<int>& nums, int target) {
+    std::map<int, int> seen_vals;
+    std::vector<int> twosum;
 
+    for(unsigned int i = 0; i < nums.size(); i++){
+        int diff = target - nums[i];
+        if (seen_vals.find(diff) != seen_vals.end()) {
+            auto p = seen_vals.find(diff);
+            twosum.push_back(p->second);
+            twosum.push_back(i);
+        }
+        seen_vals.insert({nums[i], i});
+    }
+    return twosum;
+}
 
 int main(int argc, char* argv[]) {
     std::vector<int> search_arr;
@@ -34,6 +45,13 @@ int main(int argc, char* argv[]) {
     std::vector<int> result = two_sum_dbl_loop(search_arr, target);
 
     for(int i: result) {
+        std::cout<<i<<std::endl;
+    }
+
+    std::vector<int> nresult = two_sum_hash(search_arr, target);
+
+
+    for(int i: nresult) {
         std::cout<<i<<std::endl;
     }
     return EXIT_SUCCESS;
