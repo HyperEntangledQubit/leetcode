4d54ca3bf72070b819a9bd36a9d9917dfe9e8cbf
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-04 22:39:01 -0400

    Creating method for hash

--- twosum/cpp/twosum.cpp
+++ twosum/cpp/twosum.cpp
@@ -3,8 +3,7 @@
 #include <map>
 #include <vector>
 
-std::vector<int> two_sum(std::vector<int>& nums, int target) {
-    std::map<int, int> seen_vals;
+std::vector<int> two_sum_dbl_loop(std::vector<int>& nums, int target) {
     std::vector<int> twosum; // Set vector size to only two here
 
     for(unsigned int i=0; i < nums.size(); i++) {
@@ -23,11 +22,16 @@ std::vector<int> two_sum(std::vector<int>& nums, int target) {
     return twosum;
 }
 
+void two_sum_hash(std::vector<int>& nums, int target) {
+        std::map<int, int> seen_vals;
+}
+
+
 int main(int argc, char* argv[]) {
     std::vector<int> search_arr;
     search_arr =  {2, 7, 3, 99, 107, 64, -60};
     int target = -57;
-    std::vector<int> result = two_sum(search_arr, target);
+    std::vector<int> result = two_sum_dbl_loop(search_arr, target);
 
     for(int i: result) {
         std::cout<<i<<std::endl;
