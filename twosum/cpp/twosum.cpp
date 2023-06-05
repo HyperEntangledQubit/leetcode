cdde902be83a0f3ab96446f1ec6c6ee3a9d96380
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-04 21:57:50 -0400

    First steps to solving

--- twosum/cpp/twosum.cpp
+++ twosum/cpp/twosum.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello World from C++" << std::endl;
+    std::vector<int> search_arr;
+    search_arr =  {2, 7, 3, 99, 107, 64, -60};
+    for(unsigned int i=0; i < search_arr.size(); i++) {
+        std::cout << search_arr[i] << std::endl;
+    }
     return EXIT_SUCCESS;
 }
