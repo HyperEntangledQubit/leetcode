00754c81a08a7dec8faa44b660153adedf5e7de2
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2023-06-04 22:28:09 -0400

    Finished dbl loop for twosum

--- twosum/cpp/twosum.cpp
+++ twosum/cpp/twosum.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <cstdlib>
+#include <map>
 #include <vector>
+
+std::vector<int> two_sum(std::vector<int>& nums, int target) {
+    std::map<int, int> seen_vals;
+    std::vector<int> twosum; // Set vector size to only two here
+
+    for(unsigned int i=0; i < nums.size(); i++) {
+        if (twosum.size() == 2) {
+            break;
+        }
+        int diff = target - nums[i];
+        for(unsigned int j=0; j < nums.size(); j++) {
+            if ((i != j) && (nums[i] + nums[j] == target)) {
+                twosum.push_back(i);
+                twosum.push_back(j);
+            }
+
+        }
+    }
+    return twosum;
+}
 
 int main(int argc, char* argv[]) {
     std::vector<int> search_arr;
     search_arr =  {2, 7, 3, 99, 107, 64, -60};
-    for(unsigned int i=0; i < search_arr.size(); i++) {
-        std::cout << search_arr[i] << std::endl;
+    int target = -57;
+    std::vector<int> result = two_sum(search_arr, target);
+
+    for(int i: result) {
+        std::cout<<i<<std::endl;
     }
     return EXIT_SUCCESS;
 }
